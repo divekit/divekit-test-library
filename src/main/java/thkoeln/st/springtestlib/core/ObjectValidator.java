8a73c57670a79b49aa6840f68753eb07fc3bbf74
JannIntveen <jann.intveen@smail.th-koeln.de>
2021-06-18 17:03:46 +0200

    add toString to result action assertion

--- src/main/java/thkoeln/st/springtestlib/core/ObjectValidator.java
+++ src/main/java/thkoeln/st/springtestlib/core/ObjectValidator.java
@@ -12,7 +12,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
@@ -148,7 +147,7 @@ public class ObjectValidator {
             assertFalse(doesFieldContainAnnotationName(field, "JsonIgnore"));
             resultActions.andExpect(jsonPath(fieldPath).doesNotExist());
         } else if (doesAttributeExist(attributes, field.getName()) && field.get(object) != null) {
-            resultActions.andExpect(jsonPath(fieldPath).value(equalTo(field.get(object))));
+            resultActions.andExpect(jsonPath(fieldPath).value(field.get(object).toString()));
         }
     }
 
